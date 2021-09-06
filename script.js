ebf7b6c587581a2e3876f24f6e3b8cfe91211387
rkpandeykp <90195837+rkpandeykp@users.noreply.github.com>
2021-09-06 18:36:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if(parseInt(principal)<=0)              //if input is negative
+     alert("Enter a positive number");
+    else if(principal=="")                  //if input is empty
+     alert("please enter the principal amount");
+    else
+    {
+        var rate = document.getElementById("rate").value;   
+        var years = document.getElementById("years").value;
+        var interest=principal*years*rate/100; //si = (P * R * T)/100
+        var year = new Date().getFullYear()+parseInt(years); //year when amount will be received
+        amount=parseInt(principal)+interest;                
+        document.getElementById("result").innerHTML="If you deposit\<span>\ "+principal+" \</span>\,\<br\>at an interest rate of \<span>\ "+rate+" \</span>\%\<br\>You will receive an amount of \<span>\ "+amount+",\</span>\<br\>in the year \<span>\ "+year+"\</span>\.\<br\>"
+    }
+   
+}
+function updateRate() // to update rate value according to slider in id rate_val
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+" %";
 }
         
